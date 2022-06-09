ba2f1930fd87e6ba58d40d91be2fb6c28204528f
Jepsuli <hanninen.jerejohannes@gmail.com>
2022-06-09 16:34:00 +0300

    torstai

--- Oppikirja/Program.cs
+++ Oppikirja/Program.cs
@@ -1,79 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Oppikirja
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            List<Topic> topic = new List<Topic>();
+            // List<Topic> topic = new List<Topic>();
             Topic topi = new Topic();
+            
 
-            
-                
-            
-            
-            
+
             Console.WriteLine("Anna aiheen tunniste");
-                topi.Id = Convert.ToInt32(Console.ReadLine());
+            topi.Id = Convert.ToInt32(Console.ReadLine());
+            //topic.Add();
+            Console.WriteLine("Aiheen otsikko");
+            topi.Title = Console.ReadLine();
 
-                Console.WriteLine("Aiheen otsikko");
-                topi.Title = Console.ReadLine();
+            Console.WriteLine("Anna aiheen kuvaus");
+            topi.Description = Console.ReadLine();
 
-                Console.WriteLine("Anna aiheen kuvaus");
-                topi.Description = Console.ReadLine();
+            Console.WriteLine("Anna aika arvio h.m");
+            topi.EstimatedTimeToMaster = Convert.ToDouble(Console.ReadLine());
 
-                Console.WriteLine("Anna aika arvio h.m");
-                topi.EstimatedTimeToMaster = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Kauanko olet opiskellut h.m");
+            topi.TimeSpent = Convert.ToDouble(Console.ReadLine());
 
-                Console.WriteLine("Kauanko olet opiskellut h.m");
-                topi.TimeSpent = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Anna mahdollinen lähde: ");
+            topi.Source = Console.ReadLine();
+           
+                Console.WriteLine("Anna aloitus aika: dd/MM/yyyy ");
+                topi.StartLearningDate = Convert.ToDateTime(Console.ReadLine());
 
-                Console.WriteLine("Anna mahdollinen lähde: ");
-                topi.Source = Console.ReadLine();
-
-                Console.WriteLine("Anna aloitus aika: yyyy, mm, dd ");
-                topi.StartLearningDate = Convert.ToDateTime(Console.ReadLine()); 
-
-            Console.WriteLine("Onko opiskelu kesken? Y/N");
+                Console.WriteLine("Onko opiskelu kesken? Y/N");
                 var valinta = Console.ReadLine();
-             
+          
 
                 if (valinta == "N")
                 {
-                topi.InProgress = false;
-                    Console.WriteLine("Milloin sait valmiiksi? yyyy, mm, dd");
+                    topi.InProgress = false;
+                    Console.WriteLine("Milloin sait valmiiksi? dd/MM/yyyy");
                     topi.CompletionDate = Convert.ToDateTime(Console.ReadLine());
-              
 
-                Console.WriteLine(topi.Id + "\n" + topi.Title + "\n" + topi.Description + "\n" + topi.EstimatedTimeToMaster + "\n"
-                 + topi.TimeSpent + "\n" + topi.Source + "\n" + topi.StartLearningDate + "\n" + topi.CompletionDate + "\n" + topi.InProgress);
+
+                    Console.WriteLine(topi.Id + "\n" + topi.Title + "\n" + topi.Description + "\n" + topi.EstimatedTimeToMaster + "\n"
+                     + topi.TimeSpent + "\n" + topi.Source + "\n" + topi.StartLearningDate + "\n" + topi.CompletionDate + "\n" + topi.InProgress);
 
                 }
 
-                if(valinta == "Y")
+                if (valinta == "Y")
                 {
-                topi.InProgress = true;
-                Console.WriteLine(topi.Id + "\n" + topi.Title + "\n" + topi.Description + "\n" + topi.EstimatedTimeToMaster + "\n"
-                 + topi.TimeSpent + "\n" + topi.Source + "\n" + topi.StartLearningDate + "\n" + topi.InProgress);
-                
-            }
+                    topi.InProgress = true;
+                    Console.WriteLine(topi.Id + "\n" + topi.Title + "\n" + topi.Description + "\n" + topi.EstimatedTimeToMaster + "\n"
+                     + topi.TimeSpent + "\n" + topi.Source + "\n" + topi.StartLearningDate + "\n" + topi.InProgress + "\n" + topi.CompletionDate);
 
-            else { }
+                }
+
+                else { }
+            
+            //Topic.Method();
+            
 
             String path = @"C:\Users\Jere\source\repos\Oppikirja\Id.txt";
-            File.AppendAllText(path, Convert.ToString(topi.Id + "\n" + topi.Title + "\n" + topi.Description + "\n" + topi.EstimatedTimeToMaster + "\n"
-                + topi.TimeSpent + topi.Source + "\n" + topi.StartLearningDate + "\n" + topi.InProgress + "\n" + topi.CompletionDate) + Environment.NewLine);
-            StreamReader sr = new StreamReader(@"C:\Users\Jere\source\repos\Shopping\Shoppinglist.txt");
-
-
+            string a =  Convert.ToString(topi.Id+"," + topi.Title + "," + topi.Description + "," + topi.EstimatedTimeToMaster + "," +
+                    +topi.TimeSpent + "," + topi.Source + "," + topi.StartLearningDate + "," + topi.InProgress + "," + topi.CompletionDate);
             
-                
-        }
+            File.AppendAllText(path, a + Environment.NewLine);
+            StreamReader sr = new StreamReader(@"C:\Users\Jere\source\repos\Shopping\Shoppinglist.txt");
+            //to be continue
+
+
+
         }
     }
    
-    
-    
+}
+
+
+
+
---
+++ Oppikirja/Topic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Oppikirja
+{
+    public class Topic
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public double EstimatedTimeToMaster { get; set; }
+        public double TimeSpent { get; set; }
+        public string Source { get; set; }
+        public DateTime StartLearningDate { get; set; }
+        public bool InProgress { get; set; }
+        public DateTime CompletionDate { get; set; }
+
+        /*public static void Method()
+        {
+          
+            String path = @"C:\Users\Jere\source\repos\Oppikirja\Id.txt";
+            string a = Convert.ToString(Id + "," + Title + "," + Description + "," + topi.EstimatedTimeToMaster + "," +
+                    +topi.TimeSpent + "," + topi.Source + "," + topi.StartLearningDate + "," + topi.InProgress + "," + topi.CompletionDate);
+
+            File.AppendAllText(path, a + Environment.NewLine);
+            StreamReader sr = new StreamReader(@"C:\Users\Jere\source\repos\Shopping\Shoppinglist.txt");*/
+        }
+    }
+
